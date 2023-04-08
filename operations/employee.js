0b7b4727d19913d43b3d8d319283b1ced055367d
Hashimjasem <you@example.com>
2023-04-08 16:48:09 +0800

    employee operations

---
+++ operations/employee.js
@@ -0,0 +1,33 @@
+const {connect} = require("../db/connect");
+
+
+async function getEmployee(){
+    const query = "SELECT * FROM employees";
+
+    const connection = await connect()
+
+    const [rows, cols] = await connection.query(query);
+
+    return rows;
+}
+
+
+//create employee
+async function createEmployee(firstName, lastName, roleId, managerId) {
+    const query = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+    const connection = await connect();
+    return connection.query(query, [firstName, lastName, roleId, managerId]);
+}
+
+async function updateEmployeeRole(employeeId, roleId) {
+    const query = `UPDATE employees SET role_id = ? WHERE id = ?`;
+    const connection = await connect();
+    return connection.query(query, [roleId, employeeId]);
+}
+
+
+module.exports = {
+    createEmployee,
+    getEmployee,
+    updateEmployeeRole
+}
